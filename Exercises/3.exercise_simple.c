1de550395afb1a19d7e19dee9c0eb64b59d2004d
sudenazust <119416042+sudenazust@users.noreply.github.com>
2022-11-29 15:17:31 +0300

    Update 3.exercise_simple.c

--- Exercises/3.exercise_simple.c
+++ Exercises/3.exercise_simple.c
@@ -1,2 +1,18 @@
 // Take a single character  as input, you may benefit from scanf("%c", &ch ); and print("%c", ch) to print the character. 
 // Write a program to print the ASCII value of the given character.
+#include "stdio.h"
+#include "stdlib.h"
+#include"string.h"
+#include"ctype.h"
+
+int main(){
+  char a;
+  
+  printf("enter the value");
+  scanf("%c",&a);
+  
+  printf("değerin ASCII karsiliği %c=%d",a,a);
+  return 0;
+  
+  
+}
